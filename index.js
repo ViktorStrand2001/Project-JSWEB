18dffbf88a94d1598ab1ac8a801caba0e295c2ca
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-02-03 16:25:09 +0100

    new changes in my project, product to database

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 "use strict";
+
 // HTML - Elements
-// header
-let productEl = document.getElementById("product");
-let buyernameEl = document.getElementById("buyername");
-let emailEl = document.getElementById("email");
-let shipmentEl = document.getElementById("shipment")
-let submitbuttonEl = document.getElementById("submitbutton"); 
-// main
 let headerEl = document.getElementById("headers");
 let mainEl = document.getElementById("mains");
 let footerEl = document.getElementById("footers");
-
-
-
-
 
 // Get data from API
 fetch("https://fakestoreapi.com/products")
     .then(res => res.json())
     .then(fakedata => view(fakedata));
 
-// Event listener
-// if (!buyername || ) {
-//     alert("fill all forms")
-// } else {
+// Header Function
+function headerfunction() {
+    headerEl.innerHTML += `
+    <form>
+      <label for="name">Name:</label><br>
+      <input type="text" name="buyername" id="buyername" class="forminput" placeholder="t ex Viktor Strand">
+      <br>
+      <label for="email">Email:</label><br>
+      <input type="text" name="email" id="email" class="forminput" placeholder="t ex viktor@gmail.com">
+      <br>
+      <input type="text" id="showitem" class="forminput" disabled>
+      <br>
+      <select name="shipment" id="shipment" class="forminput">
+        <option value="fast shipment">Fast Shipment</option> 
+        <option value="standard shipment">Standard Shipment</option>
+      </select>
+      <br>
+      <input type="submit" value="submit" id="submitbutton">
+      </form>
+      <hr>
+    `
+}
+headerfunction();
 
-// }
+// element - headers
+let buyernameEl = document.getElementById("buyername");
+let emailEl = document.getElementById("email");
+let shipmentEl = document.getElementById("shipment")
+let submitbuttonEl = document.getElementById("submitbutton"); 
+let showitemEl = document.getElementById("showitem");
+let forminputEl = document.getElementsByClassName("forminput");
+
+//button disabled
+
+
+
+
+
+// list item function
+function listitem(item) {
+    console.log(item);
+    showitemEl.value = item;
+}
 
 // Function main
 function view(output) {
@@ -40,14 +67,12 @@ function view(output) {
         <p> ${output[i].description} </p>
         <p> Average rating: ${output[i].rating.rate} of: ${output[i].rating.count} customer.</p>
         <p> Pris: ${output[i].price}kr </p>
-        <button type="button" class="btn btn-primary btn-lg" id="product" ${output[i].id}>Buy</button>
+        <button type="button" class="btn btn-primary btn-lg" id="item" onclick="listitem(${output[i].id})">Buy</button>
         
         <hr>
         </articale>
         `
-    }
-
-    
+    }  
 }
 
 // Function header
@@ -55,7 +80,7 @@ function createBuyer(){
     let buyername = buyernameEl.value;
     let email = emailEl.value;
     let shipment = shipmentEl.value;
-    // let product = output.id;
+    let shopitem = showitemEl.value;
 
     // Buyer value to JSON
     let body = JSON.stringify({
@@ -68,7 +93,7 @@ function createBuyer(){
                 "stringValue": shipment
             },
             "product": {
-                "stringValue": product
+                "stringValue": shopitem
             },
             "email": {
                 "stringValue": email
